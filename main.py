14ef81bf68f44d60a2f4b85d5e1019d6ed0729ed
Xanos <74895133+Xanops@users.noreply.github.com>
2021-03-12 00:34:28 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route('/list_prof/<list>')
+def index(list):
+    sp = ['инженер-исследователь', "пилот", "строитель", "экзобиолог", "врач", "инженер по терраформированию",
+          "климатолог",
+          "специалист по радиационной защите", "астрогеолог", "гляциолог", "инженер  жизнеобеспечиния", "метеоролог",
+          "оператор марсохода", "киберинженер", "штурман", "пилот дронов"]
+    if list == 'ol':
+        return render_template('index.html', arg=list, sp=sp)
+    elif list == 'ul':
+        return render_template('index.html', arg=list, sp=sp)
+    else:
+        return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
