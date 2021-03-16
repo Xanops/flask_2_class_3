a14c84fdfcb2bcb0da8775525814ef4902e05763
Xanos <74895133+Xanops@users.noreply.github.com>
2021-03-16 17:55:22 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,18 +4,23 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
+@app.route('/<title>')
+@app.route('/index/<title>')
+def index(title):
+    return render_template('index.html', title=title)
+
+
 @app.route('/list_prof/<list>')
-def index(list):
+def loop(list):
     sp = ['инженер-исследователь', "пилот", "строитель", "экзобиолог", "врач", "инженер по терраформированию",
-          "климатолог",
-          "специалист по радиационной защите", "астрогеолог", "гляциолог", "инженер  жизнеобеспечиния", "метеоролог",
-          "оператор марсохода", "киберинженер", "штурман", "пилот дронов"]
+          "климатолог", "специалист по радиационной защите", "астрогеолог", "гляциолог", "инженер  жизнеобеспечиния",
+          "метеоролог", "оператор марсохода", "киберинженер", "штурман", "пилот дронов"]
     if list == 'ol':
-        return render_template('index.html', arg=list, sp=sp)
+        return render_template('sp.html', arg=list, sp=sp)
     elif list == 'ul':
-        return render_template('index.html', arg=list, sp=sp)
+        return render_template('sp.html', arg=list, sp=sp)
     else:
-        return render_template('index.html')
+        return render_template('sp.html')
 
 
 if __name__ == '__main__':
